79ab17f8c4574c5081ab6e0d19f6f62328702ba0
Le Viet Hung <hunglv@ubuntu.(none)>
2013-10-11 10:51:40 -0700

    add 110103 solution

---
+++ 110103.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+float *a;// dynamic array declaration
+void process(float *p, int length);
+float round2f(float a);// round a float number upto 2 digits
+FILE *ofp; //= fopen("110103.uop", "w");
+
+int main(int argc, char *argv[]){
+ char *filename = argv[1];
+ FILE *fp = fopen(filename, "r");
+ ofp = fopen("110103.oup","w");
+ int count = -1;
+ //fscanf(fp,"%d",&count);
+ while(count != 0){
+    fscanf(fp, "%d",&count);
+    a = (float*) malloc(count*sizeof(float));
+    int i = 0;
+    for (i = 0; i < count; i++){
+	float temp;
+	fscanf(fp,"%f",&temp);
+	a[i] = temp;
+    }
+    if (count == 0) break;
+    process(a,count); 
+    memset(a,0,sizeof(a)); 
+ }
+ //printf("input file: %s\n", filename);
+ fclose(fp);
+ fclose(ofp);
+ 
+}
+
+void process(float *p, int length){
+ printf("numelements: %d\n", length);
+ int i = 0;
+ float sum = 0.0; 
+ float avg = 0.0;
+ for (i = 0; i < length; i++){
+   printf("element at %d: %.2f\n",i,*(p+i));
+   sum += *(p+i);
+ }
+ avg = round2f(sum / length);
+ printf("avg: %f\n", avg);
+ float exchange = 0.0f;
+ float gamount = 0.0f;
+ float ramount = 0.0f;
+ if (sum > avg*length){
+   for (i = 0; i < length; i++){
+	if(*(p+i) > avg){
+	    gamount += *(p+i) - avg;
+        }else {
+	    ramount += avg - *(p+i);
+	}
+  }
+  exchange = ramount;
+  //printf("give amount : %f\n", gamount);
+  //printf("recieve amount: %f\n", ramount);
+ }else {
+   for (i = 0; i < length; i++){
+	if(*(p+i) > avg){
+	  exchange += *(p+i)-avg;
+	}
+    }
+ }
+ //printf("exchange:%0.2f\n",exchange);
+ fprintf(ofp, "$%.2f\n",exchange);
+
+}
+
+float round2f(float a){
+ int b = (int)(a*100.0f);
+ return b/100.0f; 
+}
