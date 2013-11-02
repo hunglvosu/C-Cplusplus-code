f6b76ef3586e1b2975e9fccb2b2c41649848ccb2
Hung V. Le <hunglv.k52tncntt@gmail.com>
2013-11-01 18:16:36 -0700

    Update NKBUS.c

--- NKBUS.c
+++ NKBUS.c
@@ -5,8 +5,6 @@
 void printdata();
 void init();
 void solve();
-int *qsort(int *arr, int n);
-void swap(int *a, int *b);
 
 int **data;
 int **F;
@@ -25,7 +23,7 @@ int main(){
    for(j = 0; j < m[i]; j++){
 	scanf("%d", &data[i][j]);
   }
-  data[i] = qsort(data[i], m[i]);
+  
  }
  printdata();
  //init(); 
@@ -49,51 +47,7 @@ void solve(){
   
 }
 
-int *qsort(int *arr, int n){
-if (n < 2) {
-// the array contains 1 element or smaller
-return arr;
 
-} else {
-
-// pivot is the first element of the array
-int pivot = *arr;
-
-int i = 0;
-int k = 0;
-for (i = 1; i < n; i++){
- if (*(arr+i)< pivot){
-        if(k>0){
-                swap(arr+i, arr+k);
-                k++;
-   }
-  } else {
-        if (k==0){
-                k = i;
-        }
-  }
-}
-i--;
-if ( k > 0){
-   swap(arr+k-1, arr);
-   pivot = k-1;
-} else {
-   swap(arr+i, arr);
-   pivot = i;
-}
-//sort the left array
-qsort(arr,pivot);
-// sort the right array
-qsort(arr+pivot+1, n-pivot-1);
-
-return arr;
- }
-}
-void swap(int *a, int *b){
-   int x = *a;
-   *a = *b;
-   *b = x;
-}
 
 void printdata(){
  int i = 0,j = 0;
